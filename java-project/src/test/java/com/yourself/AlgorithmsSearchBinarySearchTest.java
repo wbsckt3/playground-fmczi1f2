bbec5ef822c40a339557199dbb7844d12506b6f6
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-07-20 22:07:42 -0500

    Update AlgorithmsSearchBinarySearchTest.java

--- java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
+++ java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
@@ -16,33 +16,5 @@ public class AlgorithmsSearchBinarySearchTest {
         Assert.assertEquals(-1, AlgorithmsSearchBinarySearch.Search(items, 1));
     }
 
-    @Test
-    private static void testSearchFound() {
-        int[] items = { 2, 3, 5, 7, 11, 13, 17 };
-        Assert.assertEquals(3, AlgorithmsSearchBinarySearch.Search(items, 7));
-    }
 
-    @Test
-    private static void testSearchNotFoundLargeArray() {
-        int[] items = { 2, 3, 5, 7, 11, 13, 17 };
-        Assert.assertEquals(-1, AlgorithmsSearchBinarySearch.Search(items, 19));
-    }
-
-    @Test
-    private static void testSearchPerformance() {
-        int[] items = new int[10000000];
-        for (int i = 0; i < items.length; i++) {
-            items[i] = i;
-        }
-        int count = 100;
-
-        Date start = new Date();
-        for (int i = 0; i < count; i++)
-            AlgorithmsSearchBinarySearch.Search(items, 7777777);
-
-        long milliseconds = (new Date()).getTime() - start.getTime();
-        System.out.println(milliseconds);
-        // Here you can add an assert statement if you want to check the performance.
-        // For simplicity, we won't include it in this example.
-    }
 }
