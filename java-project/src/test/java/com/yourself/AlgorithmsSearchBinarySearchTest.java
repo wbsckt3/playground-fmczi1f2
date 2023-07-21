4269c7f2c4e69252e34e9472f8025f25985b5ccd
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-07-20 22:49:26 -0500

    Update AlgorithmsSearchBinarySearchTest.java

--- java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
+++ java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Date;
 
 public class AlgorithmsSearchBinarySearchTest {
 
@@ -13,22 +14,26 @@ public class AlgorithmsSearchBinarySearchTest {
         try {
             int[] sortedArray = { 2, 3, 5, 7, 11, 13, 17 };
 
-            // Test cases for search values that exist in the array
-            Assert.assertEquals("Search for 2", 0, AlgorithmsSearchBinarySearch.Search(sortedArray, 2));
-            Assert.assertEquals("Search for 7", 3, AlgorithmsSearchBinarySearch.Search(sortedArray, 7));
-            Assert.assertEquals("Search for 17", 6, AlgorithmsSearchBinarySearch.Search(sortedArray, 17));
+        	// Test cases for search values that exist in the array
+        	Assert.assertEquals("Search for 2", 0, AlgorithmsSearchBinarySearch.Search(sortedArray, 2));
+        	Assert.assertEquals("Search for 7", 3, AlgorithmsSearchBinarySearch.Search(sortedArray, 7));
+        	Assert.assertEquals("Search for 17", 6, AlgorithmsSearchBinarySearch.Search(sortedArray, 17));
 
-            // Test cases for search values that do not exist in the array
-            Assert.assertEquals("Search for 1", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 1));
-            Assert.assertEquals("Search for 9", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 9));
-            Assert.assertEquals("Search for 19", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 19));
-            success(true);
+        	// Test cases for search values that do not exist in the array
+        	Assert.assertEquals("Search for 1", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 1));
+        	Assert.assertEquals("Search for 9", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 9));
+        	Assert.assertEquals("Search for 19", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 19));
+  
+            	success(true);
+		
         } catch (AssertionError ae) {
-            Assert.fail("Oops! 🐞 " + ae.getMessage() + "\nHint 💡 Did you properly accumulate all stars into 'totalStars'? 🤔");
-        }
+		success(false);
+		msg("Oops! 🐞", ae.getMessage());
+		msg("Hint 💡", "Did you properly accumulate all stars into 'totalStars'? 🤔");
+	}  
     }
 
-    private static void msg(String channel, String msg) {
+	private static void msg(String channel, String msg) {
 		System.out.println(String.format("TECHIO> message --channel \"%s\" \"%s\"", channel, msg));
 	}
 
@@ -36,17 +41,19 @@ public class AlgorithmsSearchBinarySearchTest {
 		System.out.println(String.format("TECHIO> success %s", success));
 	}
 
-    // check if a string exists in a text file
-    private static boolean existsInFile(String str, File file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
-        try {
-            while (scanner.hasNextLine()) {
-                if (scanner.nextLine().contains(str))
-                    return true;
-            }
-            return false;
-        } finally {
-            scanner.close();
-        }
-    }
+	// check if a string exists in a text file
+	private static boolean existsInFile(String str, File file) throws FileNotFoundException {
+		Scanner scanner = new Scanner(file);
+		try {
+			while (scanner.hasNextLine()) {
+				if (scanner.nextLine().contains(str))
+					return true;
+			}
+			return false;
+		} finally {
+			scanner.close();
+		}
+	}
+
+
 }
