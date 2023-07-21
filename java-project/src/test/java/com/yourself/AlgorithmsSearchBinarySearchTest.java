e60219a411826e2077f66228ff72a90763870c8e
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-07-20 22:36:31 -0500

    Update AlgorithmsSearchBinarySearchTest.java

--- java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
+++ java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
@@ -12,8 +12,19 @@ public class AlgorithmsSearchBinarySearchTest {
     @Test
     private void test() throws FileNotFoundException {
         try {
-            Assert.assertEquals(-1, AlgorithmsSearchBinarySearch.Search(items, 1));
-            success(true);
+            int[] sortedArray = { 2, 3, 5, 7, 11, 13, 17 };
+
+        	// Test cases for search values that exist in the array
+        	Assert.assertEquals("Search for 2", 0, AlgorithmsSearchBinarySearch.Search(sortedArray, 2));
+        	Assert.assertEquals("Search for 7", 3, AlgorithmsSearchBinarySearch.Search(sortedArray, 7));
+        	Assert.assertEquals("Search for 17", 6, AlgorithmsSearchBinarySearch.Search(sortedArray, 17));
+
+        	// Test cases for search values that do not exist in the array
+        	Assert.assertEquals("Search for 1", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 1));
+        	Assert.assertEquals("Search for 9", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 9));
+        	Assert.assertEquals("Search for 19", -1, AlgorithmsSearchBinarySearch.Search(sortedArray, 19));
+  
+            	success(true);
 		
         } catch (AssertionError ae) {
 		success(false);
