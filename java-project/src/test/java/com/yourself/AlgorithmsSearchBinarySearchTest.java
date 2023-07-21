7d214f99be39304849293a50c078bbdf8a500cec
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-07-20 22:09:53 -0500

    Update AlgorithmsSearchBinarySearchTest.java

--- java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
+++ java-project/src/test/java/com/yourself/AlgorithmsSearchBinarySearchTest.java
@@ -4,16 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import java.util.Date;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-@RunWith(JUnit4.class)
 public class AlgorithmsSearchBinarySearchTest {
 
     @Test
     private static void testSearchNotFound() {
         int[] items = { 2, 3, 5, 7, 11, 13, 17 };
-        Assert.assertEquals(-1, AlgorithmsSearchBinarySearch.Search(items, 1));
+        Assert.assertEquals(-1, Search(items, 1));
     }
 
 
