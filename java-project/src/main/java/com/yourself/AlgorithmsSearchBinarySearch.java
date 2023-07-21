7261771bd13ab923d3f1840aea325eb025831f1f
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-07-20 21:33:34 -0500

    Update AlgorithmsSearchBinarySearch.java

--- java-project/src/main/java/com/yourself/AlgorithmsSearchBinarySearch.java
+++ java-project/src/main/java/com/yourself/AlgorithmsSearchBinarySearch.java
@@ -5,22 +5,19 @@ import java.util.Arrays;
 
 public class AlgorithmsSearchBinarySearch {
 // }
-
     //works when the array is sorted
     int Search(int[] arr, int x){
-    int i = 0;
-    int j = arr.length;
-    while (i != j)
-    {
-    int m = (i + j)/2;
-    if (x == arr[m])
-    return m;
-    if (x < arr[m])
-    j = m;
-    else
-    i = m + 1;
-    }
-    return -1;
+        int i = 0;
+        int j = arr.length;
+        while (i != j){
+            int m = (i + j)/2;
+            if (x == arr[m]) return m;
+            if (x < arr[m]) 
+                j = m;
+            else
+                i = m + 1;
+        }
+        return -1;
     }
 
 }
