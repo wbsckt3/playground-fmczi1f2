a8889228ee24107f3cb9a5cd356edd0327baf09e
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-08-04 22:11:23 -0500

    Create ArraysAndCollectionsArrayWithDefaultValue.java

---
+++ java-project/src/main/java/com/yourself/ArraysAndCollectionsArrayWithDefaultValue.java
@@ -0,0 +1,11 @@
+//Array with a default value
+import java.util.*;
+
+var value = 5;
+var count = 3;
+var array = new int[count];   
+Arrays.fill(array, value);
+//array is { 5, 5, 5 }
+
+System.out.println(Arrays.toString(array));
+Try it in Playground
