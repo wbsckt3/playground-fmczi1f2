966593b92125bff35f0ea165421854108d384596
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-08-04 23:17:02 -0500

    Create ArraysAndCollectionsArrayMerging.java

---
+++ java-project/src/main/java/com/yourself/ArraysAndCollectionsArrayMerging.java
@@ -0,0 +1,10 @@
+//Arrays merging
+
+let firstNumbers = [ 2, 3, 5 ]
+let secondNumbers = [ 7, 11, 13 ]
+
+let allNumbers = firstNumbers + secondNumbers
+//allNumbers is [2, 3, 5, 7, 11, 13]
+
+print("allNumbers is \(allNumbers)")
+Try it in Playground
