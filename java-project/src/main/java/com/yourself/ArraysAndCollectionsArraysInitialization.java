6bc7f973cd9623eb69833881b2e230697b52138c
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2023-08-04 22:15:21 -0500

    Create  ArraysAndCollectionsArraysInitialization.java

---
+++ java-project/src/main/java/com/yourself/ArraysAndCollectionsArraysInitialization.java
@@ -0,0 +1,24 @@
+//Arrays initialization
+
+//Empty array
+let n1: [Int] = []
+let n2 = [Int]()
+
+// Single-dimensional array
+let n3: [Int] = [ 1, 2, 3 ]
+let n4 = [ 1, 2, 3 ]
+let s1 = [ "1", "2", "3" ]
+
+// Multidimensional array.
+let n5: [[Int]] = [[1, 2], [3, 4, 5]]
+var n6 = [[1, 2], [3, 4, 5]]
+n6[1][2] = 7
+
+print("n1 is \(n1)")
+print("n3 is \(n3)")
+print("n2 is \(n2)")
+print("s1 is \(s1)")
+print("n4 is \(n4)")
+print("n5 is \(n5)")
+print("n6 is \(n6)")
+Try it in Playground
